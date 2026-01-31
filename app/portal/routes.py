6dc31d92a874a998e387d65bf72e3818fa46acaa
Het Patel <hetpatel@Hets-MacBook-Air.local>
2026-02-01 02:53:27 +0530

    Professionalized entire customer portal UI and implemented consistent linking

--- app/portal/routes.py
+++ app/portal/routes.py
@@ -2,25 +2,25 @@ from flask import render_template
 from app.portal import bp
 
 @bp.route('/')
-def index():
-    return render_template('layout.html')
-
 @bp.route('/home')
 def home():
-    return render_template('portal/home.html')
+    return render_template('portal/user.html')
 
 @bp.route('/invoices')
 def invoices_list():
-    return render_template('admin/invoices_list.html')
+    return render_template('portal/invoice_list.html')
 
 @bp.route('/orders')
 def so_list():
-    return render_template('admin/so_list.html')
+    return render_template('portal/so_list.html')
 
 @bp.route('/invoice/<int:invoice_id>')
 def invoice_detail(invoice_id):
-    return "Invoice Detail Page"
+    return render_template('portal/invoice_detail.html', invoice_id=invoice_id)
 
 @bp.route('/invoice/<int:invoice_id>/pay')
 def invoice_pay(invoice_id):
-    return "Invoice Payment Page"
+    return render_template('portal/invoice_pay.html', invoice_id=invoice_id)
+@bp.route('/payments')
+def payment_history():
+    return render_template('portal/payment.html')
